dpb587/ssoca

commit 5ecaeba35728b211a802f4d117f6f6be751caa9a

yaml-unmarshalable configs; pre-Config()

service/openvpn/server/service_factory.go
@@ -1,9 +1,6 @@
 package server
 
 import (
-	"strings"
-	"time"
-
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
 
 	"github.com/dpb587/ssoca/certauth"
@@ -29,36 +26,12 @@ func (f ServiceFactory) Type() string {
 
 func (f ServiceFactory) Create(name string, options map[string]interface{}) (service.Service, error) {
 	var cfg svcconfig.Config
+	cfg.CertAuth = certauth.NewConfigValue(f.caManager)
 
 	err := config.RemarshalYAML(options, &cfg)
 	if err != nil {
 		return nil, bosherr.WrapError(err, "Loading config")
 	}
 
-	err = f.validateConfig(&cfg)
-	if err != nil {
-		return nil, bosherr.WrapError(err, "Validating config")
-	}
-
 	return NewService(name, cfg), nil
-}
-
-func (f ServiceFactory) validateConfig(config *svcconfig.Config) error {
-	config.Profile = strings.TrimSpace(config.Profile)
-
-	ca, err := f.caManager.Get(config.CertAuthName)
-	if err != nil {
-		return bosherr.WrapError(err, "Getting certificate authority")
-	}
-
-	config.CertAuth = ca
-
-	duration, err := time.ParseDuration(config.ValidityString)
-	if err != nil {
-		return bosherr.WrapError(err, "Parsing validity duration")
-	}
-
-	config.Validity = duration
-
-	return nil
 }

authn/github/server/service_factory.go
@@ -32,15 +32,14 @@ func (f ServiceFactory) Type() string {
 
 func (f ServiceFactory) Create(name string, options map[string]interface{}) (service.Service, error) {
 	var cfg svcconfig.Config
+	cfg.JWT.Validity = 24 * time.Hour
+	cfg.JWT.ValidityPast = 2 * time.Second
+	cfg.AuthURL = "https://github.com/login/oauth/authorize"
+	cfg.TokenURL = "https://github.com/login/oauth/access_token"
 
 	err := config.RemarshalYAML(options, &cfg)
 	if err != nil {
 		return nil, bosherr.WrapError(err, "Loading config")
-	}
-
-	err = f.validateConfig(&cfg)
-	if err != nil {
-		return nil, bosherr.WrapError(err, "Validating config")
 	}
 
 	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(cfg.JWT.PrivateKey))
@@ -71,22 +70,4 @@ func (f ServiceFactory) Create(name string, options map[string]interface{}) (ser
 	)
 
 	return NewService(name, cfg, backend), nil
-}
-
-func (f ServiceFactory) validateConfig(config *svcconfig.Config) error {
-	duration, err := time.ParseDuration(config.JWT.ValidityString)
-	if err != nil {
-		return bosherr.WrapError(err, "Parsing config jwt.validity")
-	}
-
-	config.JWT.Validity = duration
-
-	duration, err = time.ParseDuration(config.JWT.ValidityPastString)
-	if err != nil {
-		return bosherr.WrapError(err, "Parsing config jwt.validity_past")
-	}
-
-	config.JWT.ValidityPast = duration
-
-	return nil
 }

authn/google/server/service_factory.go
@@ -32,15 +32,14 @@ func (f ServiceFactory) Type() string {
 
 func (f ServiceFactory) Create(name string, options map[string]interface{}) (service.Service, error) {
 	var cfg svcconfig.Config
+	cfg.JWT.Validity = 24 * time.Hour
+	cfg.JWT.ValidityPast = 2 * time.Second
+	cfg.AuthURL = "https://accounts.google.com/o/oauth2/v2/auth"
+	cfg.TokenURL = "https://www.googleapis.com/oauth2/v4/token"
 
 	err := config.RemarshalYAML(options, &cfg)
 	if err != nil {
 		return nil, bosherr.WrapError(err, "Loading config")
-	}
-
-	err = f.validateConfig(&cfg)
-	if err != nil {
-		return nil, bosherr.WrapError(err, "Validating config")
 	}
 
 	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(cfg.JWT.PrivateKey))
@@ -75,22 +74,4 @@ func (f ServiceFactory) Create(name string, options map[string]interface{}) (ser
 	)
 
 	return NewService(name, cfg, backend), nil
-}
-
-func (f ServiceFactory) validateConfig(config *svcconfig.Config) error {
-	duration, err := time.ParseDuration(config.JWT.ValidityString)
-	if err != nil {
-		return bosherr.WrapError(err, "Parsing config jwt.validity")
-	}
-
-	config.JWT.Validity = duration
-
-	duration, err = time.ParseDuration(config.JWT.ValidityPastString)
-	if err != nil {
-		return bosherr.WrapError(err, "Parsing config jwt.validity_past")
-	}
-
-	config.JWT.ValidityPast = duration
-
-	return nil
 }

authn/http/server/service_factory.go
@@ -3,7 +3,9 @@ package server
 import (
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
 
-	svc "github.com/dpb587/ssoca/authn/uaa"
+	svc "github.com/dpb587/ssoca/authn/http"
+	svcconfig "github.com/dpb587/ssoca/authn/http/config"
+
 	"github.com/dpb587/ssoca/config"
 	"github.com/dpb587/ssoca/server/service"
 )
@@ -19,7 +21,7 @@ func (f ServiceFactory) Type() string {
 }
 
 func (sf ServiceFactory) Create(name string, options map[string]interface{}) (service.Service, error) {
-	var cfg Config
+	var cfg svcconfig.Config
 
 	err := config.RemarshalYAML(options, &cfg)
 	if err != nil {

authn/uaa/server/service_factory.go
@@ -3,7 +3,8 @@ package server
 import (
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
 
-	svc "github.com/dpb587/ssoca/authn/http"
+	svc "github.com/dpb587/ssoca/authn/uaa"
+	svcconfig "github.com/dpb587/ssoca/authn/uaa/config"
 	"github.com/dpb587/ssoca/config"
 	"github.com/dpb587/ssoca/server/service"
 )
@@ -19,7 +20,7 @@ func (f ServiceFactory) Type() string {
 }
 
 func (sf ServiceFactory) Create(name string, options map[string]interface{}) (service.Service, error) {
-	var cfg Config
+	var cfg svcconfig.Config
 
 	err := config.RemarshalYAML(options, &cfg)
 	if err != nil {

service/ssh/server/service_factory.go
@@ -31,52 +31,17 @@ func (f ServiceFactory) Type() string {
 
 func (f ServiceFactory) Create(name string, options map[string]interface{}) (service.Service, error) {
 	var cfg svcconfig.Config
+	cfg.Validity = 2 * time.Minute
+	cfg.CertAuth = certauth.NewConfigValue(f.caManager)
+	cfg.Principals = dynamicvalue.NewMultiConfigValue(f.dynamicvalueFactory)
+	cfg.Target = svcconfig.Target{
+		User: dynamicvalue.NewConfigValue(f.dynamicvalueFactory),
+	}
 
 	err := config.RemarshalYAML(options, &cfg)
 	if err != nil {
 		return nil, bosherr.WrapError(err, "Loading config")
 	}
 
-	err = f.validateConfig(&cfg)
-	if err != nil {
-		return nil, bosherr.WrapError(err, "Validating config")
-	}
-
 	return NewService(name, cfg), nil
-}
-
-func (f ServiceFactory) validateConfig(config *svcconfig.Config) error {
-	ca, err := f.caManager.Get(config.CertAuthName)
-	if err != nil {
-		return bosherr.WrapError(err, "Getting certificate authority")
-	}
-
-	config.CertAuth = ca
-
-	duration, err := time.ParseDuration(config.ValidityString)
-	if err != nil {
-		return bosherr.WrapError(err, "Parsing duration")
-	}
-
-	config.Validity = duration
-
-	if config.Target.RawUser != "" {
-		value, err := f.dynamicvalueFactory.Create(config.Target.RawUser)
-		if err != nil {
-			return bosherr.WrapError(err, "Parsing dynamic value target.user")
-		}
-
-		config.Target.User = value
-	}
-
-	for _, rawPrincipal := range config.RawPrincipals {
-		value, err := f.dynamicvalueFactory.Create(rawPrincipal)
-		if err != nil {
-			return bosherr.WrapError(err, "Parsing dynamic value principals")
-		}
-
-		config.Principals = append(config.Principals, value)
-	}
-
-	return nil
 }

authn/github/config/config.go
@@ -13,28 +13,7 @@ type Config struct {
 }
 
 type JWTConfig struct {
-	PrivateKey         string `yaml:"private_key"`
-	ValidityString     string `yaml:"validity"`
-	ValidityPastString string `yaml:"validity_past"`
-
-	Validity     time.Duration `yaml:"-"`
-	ValidityPast time.Duration `yaml:"-"`
-}
-
-func (c *Config) ApplyDefaults() {
-	if c.JWT.ValidityString == "" {
-		c.JWT.ValidityString = "24h"
-	}
-
-	if c.JWT.ValidityPastString == "" {
-		c.JWT.ValidityPastString = "5s"
-	}
-
-	if c.AuthURL == "" {
-		c.AuthURL = "https://github.com/login/oauth/authorize"
-	}
-
-	if c.TokenURL == "" {
-		c.TokenURL = "https://github.com/login/oauth/access_token"
-	}
+	PrivateKey   string        `yaml:"private_key"`
+	Validity     time.Duration `yaml:"validity"`
+	ValidityPast time.Duration `yaml:"validity_past"`
 }

authn/google/config/config.go
@@ -24,28 +24,7 @@ type ScopesCloudProjectConfig struct {
 }
 
 type JWTConfig struct {
-	PrivateKey         string `yaml:"private_key"`
-	ValidityString     string `yaml:"validity"`
-	ValidityPastString string `yaml:"validity_past"`
-
-	Validity     time.Duration `yaml:"-"`
-	ValidityPast time.Duration `yaml:"-"`
-}
-
-func (c *Config) ApplyDefaults() {
-	if c.JWT.ValidityString == "" {
-		c.JWT.ValidityString = "24h"
-	}
-
-	if c.JWT.ValidityPastString == "" {
-		c.JWT.ValidityPastString = "5s"
-	}
-
-	if c.AuthURL == "" {
-		c.AuthURL = "https://accounts.google.com/o/oauth2/v2/auth"
-	}
-
-	if c.TokenURL == "" {
-		c.TokenURL = "https://www.googleapis.com/oauth2/v4/token"
-	}
+	PrivateKey   string        `yaml:"private_key"`
+	Validity     time.Duration `yaml:"validity"`
+	ValidityPast time.Duration `yaml:"validity_past"`
 }

commit 0e14bf6179746951fcbb152c2df1a4b1ccc34f06

openvpn: fix certificate validity
Broken by 5ecaeba35728b211a802f4d117f6f6be751caa9a

service/openvpn/server/service_factory.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"time"
+
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
 
 	"github.com/dpb587/ssoca/certauth"
@@ -27,6 +29,7 @@ func (f ServiceFactory) Type() string {
 func (f ServiceFactory) Create(name string, options map[string]interface{}) (service.Service, error) {
 	var cfg svcconfig.Config
 	cfg.CertAuth = certauth.NewConfigValue(f.caManager)
+	cfg.Validity = 2 * time.Minute
 
 	err := config.RemarshalYAML(options, &cfg)
 	if err != nil {