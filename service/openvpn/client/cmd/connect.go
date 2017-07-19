1bdfa2ac245facba25c5ba5ab4388ee289a773d8
Danny Berger <dpb587@gmail.com>
2017-07-19 00:24:13 -0700

    openvpn: add reconnect option to connect cmd

--- service/openvpn/client/cmd/connect.go
+++ service/openvpn/client/cmd/connect.go
@@ -16,10 +16,10 @@ import (
 type Connect struct {
 	clientcmd.ServiceCommand
 
-	Exec string `long:"exec" description:"Path to the openvpn binary"`
-	// Reconnect bool `long:"reconnect" description:"Reconnect on connection disconnects"`
-	Sudo bool        `long:"sudo" description:"Execute openvpn with sudo"`
-	Args ConnectArgs `positional-args:"true"`
+	Exec      string      `long:"exec" description:"Path to the openvpn binary"`
+	Reconnect bool        `long:"reconnect" description:"Reconnect on connection disconnects"`
+	Sudo      bool        `long:"sudo" description:"Execute openvpn with sudo"`
+	Args      ConnectArgs `positional-args:"true"`
 
 	FS               boshsys.FileSystem
 	CmdRunner        boshsys.CmdRunner
@@ -34,73 +34,77 @@ type ConnectArgs struct {
 }
 
 func (c Connect) Execute(args []string) error {
-	var executable string
+	for {
+		var executable string
 
-	if c.Exec != "" {
-		executable = c.Exec
-	} else {
-		var err error
+		if c.Exec != "" {
+			executable = c.Exec
+		} else {
+			var err error
 
-		executable, err = c.ExecutableFinder.Find()
+			executable, err = c.ExecutableFinder.Find()
+			if err != nil {
+				return bosherr.WrapError(err, "Finding executable")
+			}
+		}
+
+		client, err := c.GetClient(c.ServiceName)
 		if err != nil {
-			return bosherr.WrapError(err, "Finding executable")
+			return bosherr.WrapError(err, "Getting client")
+		}
+
+		profileManager, err := profile.CreateManagerAndPrivateKey(client, c.ServiceName)
+		if err != nil {
+			return bosherr.WrapError(err, "Getting profile manager")
+		}
+
+		profile, err := profileManager.GetProfile()
+		if err != nil {
+			return bosherr.WrapError(err, "Getting profile")
+		}
+
+		tmpdir, err := c.FS.TempDir("openvpn")
+		if err != nil {
+			return bosherr.WrapError(err, "Creating tmpdir")
+		}
+
+		defer c.FS.RemoveAll(tmpdir)
+
+		err = c.FS.Chmod(tmpdir, 0700)
+		if err != nil {
+			return bosherr.WrapError(err, "Chmod'ing tmpdir")
+		}
+
+		configPath := fmt.Sprintf("%s/openvpn.ovpn", tmpdir)
+
+		err = c.FS.WriteFileString(configPath, profile.String())
+		if err != nil {
+			return bosherr.WrapError(err, "Writing certificate")
+		}
+
+		openvpnargs := []string{}
+
+		if c.Sudo {
+			openvpnargs = append(openvpnargs, executable)
+			executable = "sudo"
+		}
+
+		openvpnargs = append(openvpnargs, "--config", configPath)
+		openvpnargs = append(openvpnargs, c.Args.Extra...)
+
+		_, _, _, err = c.CmdRunner.RunComplexCommand(boshsys.Command{
+			Name: executable,
+			Args: openvpnargs,
+
+			Stdin:  os.Stdin,
+			Stdout: os.Stdout,
+			Stderr: os.Stderr,
+
+			KeepAttached: true,
+		})
+
+		if !c.Reconnect {
+			return err
 		}
 	}
-
-	client, err := c.GetClient(c.ServiceName)
-	if err != nil {
-		return bosherr.WrapError(err, "Getting client")
-	}
-
-	profileManager, err := profile.CreateManagerAndPrivateKey(client, c.ServiceName)
-	if err != nil {
-		return bosherr.WrapError(err, "Getting profile manager")
-	}
-
-	profile, err := profileManager.GetProfile()
-	if err != nil {
-		return bosherr.WrapError(err, "Getting profile")
-	}
-
-	tmpdir, err := c.FS.TempDir("openvpn")
-	if err != nil {
-		return bosherr.WrapError(err, "Creating tmpdir")
-	}
-
-	defer c.FS.RemoveAll(tmpdir)
-
-	err = c.FS.Chmod(tmpdir, 0700)
-	if err != nil {
-		return bosherr.WrapError(err, "Chmod'ing tmpdir")
-	}
-
-	configPath := fmt.Sprintf("%s/openvpn.ovpn", tmpdir)
-
-	err = c.FS.WriteFileString(configPath, profile.String())
-	if err != nil {
-		return bosherr.WrapError(err, "Writing certificate")
-	}
-
-	openvpnargs := []string{}
-
-	if c.Sudo {
-		openvpnargs = append(openvpnargs, executable)
-		executable = "sudo"
-	}
-
-	openvpnargs = append(openvpnargs, "--config", configPath)
-	openvpnargs = append(openvpnargs, c.Args.Extra...)
-
-	_, _, _, err = c.CmdRunner.RunComplexCommand(boshsys.Command{
-		Name: executable,
-		Args: openvpnargs,
-
-		Stdin:  os.Stdin,
-		Stdout: os.Stdout,
-		Stderr: os.Stderr,
-
-		KeepAttached: true,
-	})
-
-	return nil
 }
