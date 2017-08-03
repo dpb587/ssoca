755df0145e6a166d7a78711addc9237a146af656
Danny Berger <dberger@pivotal.io>
2017-08-02 22:06:29 -0700

    Add Windows support for openvpn connections
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- service/openvpn/client/helper/executable_finder.go
+++ service/openvpn/client/helper/executable_finder.go
@@ -7,22 +7,21 @@ import (
 	boshsys "github.com/cloudfoundry/bosh-utils/system"
 )
 
-// var tunnelblickDefault = "/Applications/Tunnelblick.app/Contents/Resources/openvpn/openvpn-2.4.1-openssl-1.0.2k/openvpn"
-var tunnelblickDefault = "/Applications/Tunnelblick.app/Contents/Resources/openvpn/default"
-
 type ExecutableFinder struct {
 	Paths []string
 	FS    boshsys.FileSystem
 }
 
 func (ef ExecutableFinder) Find() (string, error) {
-	path, err := exec.LookPath("openvpn")
+	path, err := exec.LookPath(guessExecutableName)
 	if err == nil {
 		return path, nil
 	}
 
-	if ef.FS.FileExists(tunnelblickDefault) {
-		return tunnelblickDefault, nil
+	for _, path := range guessExecutablePaths {
+		if ef.FS.FileExists(path) {
+			return path, nil
+		}
 	}
 
 	return "", errors.New("Failed to find the openvpn executable")
