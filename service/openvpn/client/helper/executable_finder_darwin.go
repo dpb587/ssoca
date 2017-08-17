cf958f83f31387251fda6aedcc24506bf4c83c2b
Danny Berger <dpb587@gmail.com>
2017-08-16 23:20:36 -0700

    Add Viscosity/Shimo embedded openvpn paths

--- service/openvpn/client/helper/executable_finder_darwin.go
+++ service/openvpn/client/helper/executable_finder_darwin.go
@@ -3,6 +3,8 @@ package helper
 var guessExecutableName = "openvpn"
 var guessExecutablePaths = []string{
 	"/Applications/Tunnelblick.app/Contents/Resources/openvpn/default",
+	"/Applications/Shimo.app/Contents/MacOS/openvpn",
+	"/Applications/Viscosity.app/Contents/MacOS/openvpn",
 }
 var guessExecutableSuggestions = `
 If you use Homebrew, you can install the openvpn formula...
