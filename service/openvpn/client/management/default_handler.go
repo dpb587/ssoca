26416f47e8583825eeb01ffcc27771ea3e4924fa
Danny Berger <dpb587@gmail.com>
2017-08-26 11:32:32 -0700

    Add failsafe for repeated openvpn certificate lookups

--- service/openvpn/client/management/default_handler.go
+++ service/openvpn/client/management/default_handler.go
@@ -3,6 +3,7 @@ package management
 import (
 	"encoding/base64"
 	"io"
+	"time"
 
 	"github.com/dpb587/ssoca/service/openvpn/client/profile"
 
@@ -11,13 +12,21 @@ import (
 
 type DefaultHandler struct {
 	profileManager profile.Manager
+
+	attempts       []time.Time
+	attemptsOffset int
+	attemptsMax    int
 }
 
 var _ ServerHandler = &DefaultHandler{}
 
 func NewDefaultHandler(profileManager profile.Manager) *DefaultHandler {
+	attemptsMax := 5
+
 	return &DefaultHandler{
 		profileManager: profileManager,
+		attempts:       make([]time.Time, attemptsMax),
+		attemptsMax:    attemptsMax,
 	}
 }
 
@@ -25,6 +34,12 @@ func (ch *DefaultHandler) NeedCertificate(w io.Writer, _ string) (ServerHandlerC
 	profile, err := ch.profileManager.GetProfile()
 	if err != nil {
 		return nil, bosherr.WrapError(err, "Retrieving profile")
+	} else if ch.exceedsRecentAttempts() {
+		// internally throttle ourselves after repeated certificate lookups
+		// typically this would mean non-expired certs are expired/invalid and should be reloaded
+		w.Write([]byte("signal SIGHUP\n"))
+
+		return SuccessCallback, nil
 	}
 
 	w.Write([]byte("certificate\n"))
@@ -60,4 +75,22 @@ func (ch *DefaultHandler) SignRSA(w io.Writer, data string) (ServerHandlerCallba
 	w.Write([]byte("END\n"))
 
 	return SuccessCallback, nil
+}
+
+func (ch *DefaultHandler) exceedsRecentAttempts() bool {
+	ch.attemptsOffset = (ch.attemptsOffset + 1) % ch.attemptsMax
+	ch.attempts[ch.attemptsOffset] = time.Now()
+
+	since := time.Now().Add(-5 * time.Minute)
+	sinceCount := 0
+
+	for _, attempt := range ch.attempts {
+		if attempt.Before(since) {
+			continue
+		}
+
+		sinceCount++
+	}
+
+	return sinceCount >= ch.attemptsMax
 }
