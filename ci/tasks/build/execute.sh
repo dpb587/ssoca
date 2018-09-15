76a58df346bd0ff475982d2d749716c3f80c9056
Danny Berger <dpb587@gmail.com>
2018-09-15 03:09:32 +0000

    Remove unused CI variable references
    Gone since 4ed502ef5bb27b3ab417d296114d9e6e583c3676

--- ci/tasks/build/execute.sh
+++ ci/tasks/build/execute.sh
@@ -5,11 +5,6 @@ set -eu
 build_dir="$PWD/build"
 version="$( cat version/version )"
 
-s3_prefix="${s3_prefix:-}"
-
-export AWS_ACCESS_KEY_ID="$s3_access_key"
-export AWS_SECRET_ACCESS_KEY="$s3_secret_key"
-
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../.."
 
 source .envrc
