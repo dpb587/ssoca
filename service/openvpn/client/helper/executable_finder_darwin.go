00618e93f377bd810317cbdbb158f2864e5cae59
Danny Berger <dpb587@gmail.com>
2019-04-08 05:20:49 -0700

    Fix formatting

--- service/openvpn/client/helper/executable_finder_darwin.go
+++ service/openvpn/client/helper/executable_finder_darwin.go
@@ -3,7 +3,7 @@ package helper
 var guessExecutableName = "openvpn"
 var guessExecutablePaths = []string{
 	"/usr/local/sbin/openvpn",
-        "/usr/sbin/openvpn",
+	"/usr/sbin/openvpn",
 	"/Applications/Tunnelblick.app/Contents/Resources/openvpn/openvpn-2.4.*-openssl-1.*/openvpn",
 	"/Applications/Shimo.app/Contents/MacOS/openvpn",
 	"/Applications/Viscosity.app/Contents/MacOS/openvpn",
