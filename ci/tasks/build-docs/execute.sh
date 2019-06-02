9a5de91bdf2d2de451b1ac4f9388ecdb2acecd86
Danny Berger <dpb587@gmail.com>
2019-06-02 10:07:01 -0700

    Add preview-docs script

--- ci/tasks/build-docs/execute.sh
+++ ci/tasks/build-docs/execute.sh
@@ -19,31 +19,14 @@ wget -qO static/img/dpb587.jpg https://dpb587.me/images/dpb587-20140313a~256.jpg
 latest_version=$( grep '^  ' data/repo/tags.yml | awk '{ print $1 }' | sed -e 's/^v//' -e 's/:$//' | sort -rV | head -n1 )
 
 cat > config.local.yml <<EOF
-title: ssoca
 baseURL: "https://dpb587.github.io/ssoca"
 googleAnalytics: "UA-37464314-3"
-theme:
-- balmy-release
-- balmy
 params:
-  ThemeBrandIcon: /img/dpb587.jpg
-  ThemeIncludeMenu: "/_menu"
-  ThemeNavBadges: []
-  ThemeNavItems:
-  - title: docs
-    url: /
-  - title: releases
-    url: /releases/
-  - title: github
-    url: "https://github.com/dpb587/ssoca"
-  GitRepo: "https://github.com/dpb587/ssoca"
-  GitEditPath: blob/master/docs
-  GitCommitPath: commit
   releaseVersionLatest: "$latest_version"
 EOF
 
 hugo \
-  --config="config.yml,config.local.yml" \
+  --config="config.yml,$task_dir/repo/docs/config.yml,config.local.yml" \
   --contentDir="$task_dir/repo/docs" \
   --destination="$task_dir/public"
 
