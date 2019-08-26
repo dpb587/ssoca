dpb587/ssoca

commit b1916d34330e4a05ace841b439be47d96286a6f9

Switch auth retry strategy to exec instead of in-memory
Switching to re-execute ssoca seems easier than finding other
ways to deal with reading from blocking STDIN. The general
workflow was auth occuring via browesr token redirect followed
by user needing to enter sudo password for openvpn.

Fixes https://github.com/dpb587/ssoca/issues/8

client/httpclient/client.go
@@ -1,34 +1,28 @@
 package httpclient
 
 import (
-	"fmt"
 	"io"
 	"net/http"
+	"os/exec"
 	"strings"
 
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
-	envclient "github.com/dpb587/ssoca/service/env/client"
 
-	"github.com/dpb587/ssoca/client/config"
-	"github.com/dpb587/ssoca/client/service"
+	ssocaclient "github.com/dpb587/ssoca/client"
 	baseclient "github.com/dpb587/ssoca/httpclient"
 )
 
 type client struct {
-	upstream        baseclient.Client
-	serviceManager  service.Manager
-	configManager   config.Manager
-	environmentName string
+	upstream baseclient.Client
+	runtime  ssocaclient.Runtime
 }
 
 var _ baseclient.Client = client{}
 
-func NewClient(upstream baseclient.Client, serviceManager service.Manager, configManager config.Manager, environmentName string) baseclient.Client {
+func NewClient(upstream baseclient.Client, runtime ssocaclient.Runtime) baseclient.Client {
 	return client{
-		upstream:        upstream,
-		serviceManager:  serviceManager,
-		configManager:   configManager,
-		environmentName: environmentName,
+		upstream: upstream,
+		runtime:  runtime,
 	}
 }
 
@@ -73,56 +67,33 @@ func (c client) attemptReauthenticate(err error) error {
 		return err
 	}
 
-	rawEnvService, err := c.serviceManager.Get("env")
+	configManager, err := c.runtime.GetConfigManager()
 	if err != nil {
-		return bosherr.WrapError(err, "Getting env service")
+		return err
 	}
 
-	envService, ok := rawEnvService.(envclient.Service)
-	if !ok {
-		return bosherr.WrapError(err, "Expecting env service")
-	}
+	// this assumes runtime has a specific CLI; probably not optimal
+	cmd := exec.Command(
+		c.runtime.GetExec(),
+		"--config", configManager.GetSource(),
+		"--environment", c.runtime.GetEnvironmentName(),
+		"auth",
+		"login",
+	)
 
-	envClient, err := envService.GetClient()
+	// It is weird for us to be shelling out instead of handling auth in process.
+	// We fork to a new process and propagate I/O because some auth supports
+	// optional stdin (e.g. server_token_retrieval.go). However, you can't really
+	// do non-blocking I/O, so if no user input was provided, it would hang in
+	// post-auth commands. Separate process indirectly fixes it by returning
+	// ownership of stdin when its done. https://github.com/dpb587/ssoca/issues/8
+	cmd.Stdin = c.runtime.GetStdin()
+	cmd.Stdout = c.runtime.GetStdout()
+	cmd.Stderr = c.runtime.GetStderr()
+
+	err = cmd.Run()
 	if err != nil {
-		return bosherr.WrapError(err, "Getting env HTTP client")
-	}
-
-	envInfo, err := envClient.GetInfo()
-	if err != nil {
-		return bosherr.WrapError(err, "Getting environment info")
-	}
-
-	authServiceType := envInfo.Auth.Type
-
-	svc, err := c.serviceManager.Get(authServiceType)
-	if err != nil {
-		return bosherr.WrapError(err, "Loading auth service")
-	}
-
-	authService, ok := svc.(service.AuthService)
-	if !ok {
-		return fmt.Errorf("Cannot authenticate with service: %s", authServiceType)
-	}
-
-	auth, err := authService.AuthLogin(envInfo.Auth)
-	if err != nil {
-		return bosherr.WrapError(err, "Authenticating")
-	}
-
-	env, err := c.configManager.GetEnvironment(c.environmentName)
-	if err != nil {
-		return bosherr.WrapError(err, "Getting environment state")
-	}
-
-	env.Auth = &config.EnvironmentAuthState{
-		Type:    authServiceType,
-		Options: auth,
-	}
-
-	err = c.configManager.SetEnvironment(env)
-	if err != nil {
-		return bosherr.WrapError(err, "Updating environment")
+		return bosherr.WrapError(err, "auth exec")
 	}
 
 	return nil

cli/client/client.go
@@ -41,7 +41,7 @@ func main() {
 	ui := boshui.NewConfUI(logger)
 	serviceManager := service.NewDefaultManager()
 
-	runtime := goflags.NewRuntime(version.MustVersion(appName, appSemver, appCommit, appBuilt), serviceManager, ui, os.Stdin, os.Stdout, os.Stderr, fs, logger)
+	runtime := goflags.NewRuntime(os.Args[0], version.MustVersion(appName, appSemver, appCommit, appBuilt), serviceManager, ui, os.Stdin, os.Stdout, os.Stderr, fs, logger)
 	var parser = flags.NewParser(&runtime, flags.Default)
 
 	authService := srv_auth.NewService(&runtime, serviceManager)

client/clientfakes/fake_runtime.go
@@ -14,6 +14,15 @@ import (
 )
 
 type FakeRuntime struct {
+	GetExecStub        func() string
+	getExecMutex       sync.RWMutex
+	getExecArgsForCall []struct{}
+	getExecReturns     struct {
+		result1 string
+	}
+	getExecReturnsOnCall map[int]struct {
+		result1 string
+	}
 	GetVersionStub        func() version.Version
 	getVersionMutex       sync.RWMutex
 	getVersionArgsForCall []struct{}
@@ -123,6 +132,46 @@ type FakeRuntime struct {
 	}
 	invocations      map[string][][]interface{}
 	invocationsMutex sync.RWMutex
+}
+
+func (fake *FakeRuntime) GetExec() string {
+	fake.getExecMutex.Lock()
+	ret, specificReturn := fake.getExecReturnsOnCall[len(fake.getExecArgsForCall)]
+	fake.getExecArgsForCall = append(fake.getExecArgsForCall, struct{}{})
+	fake.recordInvocation("GetExec", []interface{}{})
+	fake.getExecMutex.Unlock()
+	if fake.GetExecStub != nil {
+		return fake.GetExecStub()
+	}
+	if specificReturn {
+		return ret.result1
+	}
+	return fake.getExecReturns.result1
+}
+
+func (fake *FakeRuntime) GetExecCallCount() int {
+	fake.getExecMutex.RLock()
+	defer fake.getExecMutex.RUnlock()
+	return len(fake.getExecArgsForCall)
+}
+
+func (fake *FakeRuntime) GetExecReturns(result1 string) {
+	fake.GetExecStub = nil
+	fake.getExecReturns = struct {
+		result1 string
+	}{result1}
+}
+
+func (fake *FakeRuntime) GetExecReturnsOnCall(i int, result1 string) {
+	fake.GetExecStub = nil
+	if fake.getExecReturnsOnCall == nil {
+		fake.getExecReturnsOnCall = make(map[int]struct {
+			result1 string
+		})
+	}
+	fake.getExecReturnsOnCall[i] = struct {
+		result1 string
+	}{result1}
 }
 
 func (fake *FakeRuntime) GetVersion() version.Version {
@@ -580,6 +629,8 @@ func (fake *FakeRuntime) GetStdinReturnsOnCall(i int, result1 io.Reader) {
 func (fake *FakeRuntime) Invocations() map[string][][]interface{} {
 	fake.invocationsMutex.RLock()
 	defer fake.invocationsMutex.RUnlock()
+	fake.getExecMutex.RLock()
+	defer fake.getExecMutex.RUnlock()
 	fake.getVersionMutex.RLock()
 	defer fake.getVersionMutex.RUnlock()
 	fake.getEnvironmentMutex.RLock()

client/goflags/runtime.go
@@ -31,6 +31,7 @@ type Runtime struct {
 
 	Version cmd.Version `command:"version" description:"Show the current version"`
 
+	exec           string
 	version        version.Version
 	serviceManager service.Manager
 	fs             boshsys.FileSystem
@@ -46,8 +47,9 @@ type Runtime struct {
 
 var _ client.Runtime = Runtime{}
 
-func NewRuntime(version_ version.Version, serviceManager service.Manager, ui boshui.UI, stdin io.Reader, stdout io.Writer, stderr io.Writer, fs boshsys.FileSystem, logger boshlog.Logger) Runtime {
+func NewRuntime(exec string, version_ version.Version, serviceManager service.Manager, ui boshui.UI, stdin io.Reader, stdout io.Writer, stderr io.Writer, fs boshsys.FileSystem, logger boshlog.Logger) Runtime {
 	runtime := Runtime{
+		exec:           exec,
 		version:        version_,
 		serviceManager: serviceManager,
 		fs:             fs,
@@ -78,6 +80,10 @@ func (r Runtime) GetLogger() logrus.FieldLogger {
 		"cli.name":   r.Version.Version.Name,
 		"cli.semver": r.Version.Version.Semver,
 	})
+}
+
+func (r Runtime) GetExec() string {
+	return r.exec
 }
 
 func (r Runtime) GetVersion() version.Version {
@@ -161,12 +167,7 @@ func (r Runtime) GetAuthInterceptClient() (httpclient.Client, error) {
 		return nil, err
 	}
 
-	configManager, err := r.GetConfigManager()
-	if err != nil {
-		return nil, err
-	}
-
-	return authintercept.NewClient(client, r.serviceManager, configManager, r.GetEnvironmentName()), nil
+	return authintercept.NewClient(client, r), nil
 }
 
 func (r Runtime) GetConfigManager() (config.Manager, error) {

client/interfaces.go
@@ -12,6 +12,7 @@ import (
 
 //go:generate counterfeiter . Runtime
 type Runtime interface {
+	GetExec() string
 	GetVersion() version.Version
 	GetEnvironment() (config.EnvironmentState, error)
 	GetEnvironmentName() string

commit 48fa45fc24fc7678ead9726647ca23e5e8efaa45

Fix auth status being unexpectedly shown in output
Broken since b1916d34330e4a05ace841b439be47d96286a6f9

client/httpclient/client.go
@@ -92,6 +92,7 @@ func (c client) attemptReauthenticate(err error) error {
 		"auth",
 		"--service", authServiceName,
 		"login",
+		"--skip-verify", // we'll be verifying in our next request
 	)
 
 	// It is weird for us to be shelling out instead of handling auth in process.
@@ -101,6 +102,11 @@ func (c client) attemptReauthenticate(err error) error {
 	// post-auth commands. Separate process indirectly fixes it by returning
 	// ownership of stdin when its done. https://github.com/dpb587/ssoca/issues/8
 	cmd.Stdin = c.runtime.GetStdin()
+	// Warning: STDOUT emitted by the authentication process could corrupt the
+	// output of the original command (e.g. if it's being used to generate a
+	// configuration file). At the moment, the only STDOUT comes from verifying
+	// the token after authenticating, so --skip-verify is avoiding the issue. It
+	// might be better to redirect to STDERR, but that seems a little weird.
 	cmd.Stdout = c.runtime.GetStdout()
 	cmd.Stderr = c.runtime.GetStderr()
 